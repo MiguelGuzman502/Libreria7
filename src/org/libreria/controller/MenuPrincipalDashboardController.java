773620ae286ccac83a41c5930608e225c7a43278
Miguel Guzman <mguzman-2023183@kinal.edu.gt>
2026-08-26 18:33:32 -0600

    003 se completo sprint 0

--- src/org/libreria/controller/MenuPrincipalDashboardController.java
+++ src/org/libreria/controller/MenuPrincipalDashboardController.java
@@ -1,26 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package org.libreria.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import org.libreria.system.Main;
 
-/**
- * FXML Controller class
- *
- * @author informatica
- */
-public class MenuPrincipalDashboardController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+public class MenuPrincipalDashboardController {
+    @FXML
+    private BorderPane rootPane;
+    @FXML
+    private Button btnClientes;
+    @FXML
+    private Button btnAutores;
+    @FXML
+    private Button btnCategorias;
+    @FXML
+    private Button btnEditoriales;
+    @FXML
+    private Button btnSalir;
+    @FXML
+    public void handleClientes(ActionEvent event) {
+        System.out.println("Abriendo sección de Clientes...");
+    }
+    @FXML
+    public void handleAutores(ActionEvent event) {
+        System.out.println("Abriendo sección de Autores...");
+    }
+    @FXML
+    public void handleCategorias(ActionEvent event) {
+        System.out.println("Abriendo sección de Categorías...");
+    }
+    @FXML
+    public void handleEditoriales(ActionEvent event) {
+        System.out.println("Abriendo sección de Editoriales...");
+    }
+    @FXML
+    public void handleSalir(ActionEvent event) {
+        System.exit(0);
+    }
 }
