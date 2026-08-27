1e1111cc97b2ca618bfd5e3c3874ddea1ce13303
Informatica <informatica@alumno.local>
2026-08-27 11:50:49 -0600

    003 - Se agrego codigo a una nueva clase llamada Sesion

--- src/org/libreria/util/Sesion.java
+++ src/org/libreria/util/Sesion.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.libreria.util;
 
-/**
- *
- * @author informatica
- */
+import org.libreria.model.Usuario;
+
 public class Sesion {
-    
+
+    private static Sesion instancia;
+    private Usuario usuarioActual;
+
+    private Sesion() {}
+
+    public static Sesion getInstancia() {
+        if (instancia == null) {
+            instancia = new Sesion();
+        }
+        return instancia;
+    }
+
+    public Usuario getUsuarioActual() {
+        return usuarioActual;
+    }
+
+    public void iniciarSesion(Usuario usuario) {
+        this.usuarioActual = usuario;
+    }
+
+    public void cerrarSesion() {
+        this.usuarioActual = null;
+    }
+
+    public boolean haySesionActiva() {
+        return usuarioActual != null;
+    }
+
+    public boolean esAdmin() {
+        return usuarioActual != null && "ADMIN".equalsIgnoreCase(usuarioActual.getRol());
+    }
+
+    public boolean esCajero() {
+        return usuarioActual != null && "CAJERO".equalsIgnoreCase(usuarioActual.getRol());
+    }
+
+    public boolean esBodega() {
+        return usuarioActual != null && "BODEGA".equalsIgnoreCase(usuarioActual.getRol());
+    }
 }
