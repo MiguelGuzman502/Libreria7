074c37d5a969cedb450fd8b5c36f2032578e9894
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-09-03 08:45:34 -0600

    se completo la clase de los roles

--- src/org/libreria/manager/RolPermisos.java
+++ src/org/libreria/manager/RolPermisos.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.libreria.manager;
-
-/**
- *
- * @author informatica
- */
 public class RolPermisos {
-    
+
+    private RolPermisos() {
+    }
+
+    public static boolean puedeAccederAdmin() {
+        return SessionContext.esAdmin();
+    }
+
+    public static boolean puedeAccederBodega() {
+        return SessionContext.esAdmin()
+                || SessionContext.esEmpleado();
+    }
+
+    public static boolean puedeAccederCajero() {
+        return SessionContext.esAdmin()
+                || SessionContext.esCajero();
+    }
+
+    public static boolean puedeGestionarUsuarios() {
+        return SessionContext.esAdmin();
+    }
+
+    public static boolean puedeCambiarPassword() {
+        return SessionContext.sesionActiva();
+    }
+
+    public static boolean puedeCerrarSesion() {
+        return SessionContext.sesionActiva();
+    }
 }
